2494d1a258bae2af303d1584fbc9ab30d659bc06
ElSobitol <60044826+ElSobitol@users.noreply.github.com>
2023-02-21 00:57:21 +0300

    Add files via upload

---
+++ Lesson_2/js/test_script.js
@@ -0,0 +1,23 @@
+//Задача 1
+// console.log('Привет, я Толик Царапкин!');
+//Задача 2.
+// let fafaf = 10;
+// alert(`Привет, ${fafaf}`); 
+// задача 4
+// let title =("Как Вас зовут?");
+// result = prompt(title, ["Имя"]);
+// alert(`Привет,${result}`);
+
+// задача 4.1
+
+// let title = prompt("Как вас зовут?", "Имя");
+// alert(`Привет,${title}`);
+
+
+function Name1(){
+  let title = prompt("Как вас зовут?", "Имя");
+  alert(`Привет,${title}`);
+
+}
+
+Name1();
